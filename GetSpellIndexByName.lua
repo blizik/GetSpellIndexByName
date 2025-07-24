67cac71300f2fd78d764cf729cda2919f4668ef7
blizik <portalblizik@gmail.com>
2025-07-24 16:00:14 -0400

    Update GetSpellIndexByName.lua indent

--- GetSpellIndexByName.lua
+++ GetSpellIndexByName.lua
@@ -1,31 +1,31 @@
 local SpellNameToIndex = {}
 
 local function BuildSpellMap()
- for k,v in pairs(SpellNameToIndex) do
-  SpellNameToIndex[k] = nil
- end
+    for k,v in pairs(SpellNameToIndex) do
+        SpellNameToIndex[k] = nil
+    end
 
- local id = 1
- while true do
-  local name, rank = GetSpellName(id, "BOOKTYPE_SPELL")
-  if not name then
-   break
-  end
+    local id = 1
+    while true do
+        local name, rank = GetSpellName(id, "BOOKTYPE_SPELL")
+        if not name then
+            break
+        end
 
-  if rank ~= "" then
-   SpellNameToIndex[string.lower(name) .. "(" .. string.lower(rank) .. ")"] = id
-  end
-  SpellNameToIndex[string.lower(name)] = id
+        if rank ~= "" then
+            SpellNameToIndex[string.lower(name) .. "(" .. string.lower(rank) .. ")"] = id
+        end
+        SpellNameToIndex[string.lower(name)] = id
 
-  id = id + 1
- end
+        id = id + 1
+    end
 
- DEFAULT_CHAT_FRAME:AddMessage(string.format("GetSpellIndexByName: Loaded %d spells.", id - 1))
+    DEFAULT_CHAT_FRAME:AddMessage(string.format("GetSpellIndexByName: Loaded %d spells.", id - 1))
 end
 
 function GetSpellIndexByName(name)
- if next(SpellNameToIndex) == nil then
-  BuildSpellMap()
- end
- return SpellNameToIndex[string.lower(name)]
+    if next(SpellNameToIndex) == nil then
+        BuildSpellMap()
+    end
+    return SpellNameToIndex[string.lower(name)]
 end
